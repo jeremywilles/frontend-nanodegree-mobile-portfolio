55e08cf3faf0bfc9d9a57a3a84eac6b0c21baa41
Jeremy Willes <jeremywilles@gmail.com>
2015-10-05 20:22:48 -0500

    fix error on 507

--- views/js/main.js
+++ views/js/main.js
@@ -504,7 +504,7 @@ function updatePositions() {
   frame++;
   window.performance.mark("mark_start_frame");
   //replace querySelectorAll by getElementsByClassName
-  var items = document.getElementsbyClassName('.mover');
+  var items = document.getElementsByClassName('.mover');
   for (var i = 0; i < items.length; i++) {
     var phase = Math.sin((document.body.scrollTop / 1250) + (i % 5));
     items[i].style.left = items[i].basicLeft + 100 * phase + 'px';
